fe18d60642c909ea2399413fe0bf974f5f2e58d3
jshiffert <jshiffert@oki.org>
2023-08-01 18:42:35 -0400

    debug routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,10 +8,8 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
     try {
     const tagData = await Tag.findAll({
-      include: [{
-        model: ProductTag,
-        include: [Product]
-      }],
+      include: [{ model: ProductTag }],
+      include: [{model: Product}]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -24,10 +22,8 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
     try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{
-        model: ProductTag,
-        include: [Product]
-      }],
+      include: [{ model: ProductTag }],
+      include: [{model: Product}]
     });
 
     if (!tagData) {
